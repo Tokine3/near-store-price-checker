469354fb9b357a26ef47d3c68313eaa65b07dad8
Tokine3 <s.ymd425@gmail.com>
2024-12-12 20:24:33 +0900

    fix

--- apps/frontend/src/app/login/_hooks/useLogin.ts
+++ apps/frontend/src/app/login/_hooks/useLogin.ts
@@ -5,7 +5,6 @@ import {
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
-  getAuth,
 } from 'firebase/auth';
 import { auth } from '@/lib/firebase/config';
 import { api } from '@/lib/axios';
